ba45bc3a50678d6d0ecbfca6e9e302c8b7a83599
Javier Chavez <javier120699lili@gmail.com>
2021-01-06 22:31:20 -0500

    Declare constans

---
+++ js/app.js
@@ -0,0 +1,27 @@
+const colors = ["green", "red", "blue", "yellow"]
+const levels = [
+    {
+        name: "Facil",
+        pointsByRound: 1,
+        pointsToPass: 3,
+        difficulty: 3,
+    },
+    {
+        name: "Medio",
+        pointsByRound: 3,
+        pointsToPass: 9,
+        difficulty: 6,
+    },
+    {
+        name: "Difícil",
+        pointsByRound: 5,
+        pointsToPass: 20,
+        difficulty: 12,
+    },
+    {
+        name: "¡CRIMINAL!",
+        pointsByRound: 8,
+        pointsToPass: 50,
+        difficulty: 18,
+    },
+]
